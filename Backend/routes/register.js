deed02c013e326cb2972a87b29e4aade04b426e4
springik <springik.games@email.cz>
2023-11-21 17:47:10 +0100

    Added registration router ❤️
    Registration router implementation

---
+++ Backend/routes/register.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const registerRouter = express.Router();
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'podhubydb'
+});
+const registerQuery = 'INSERT INTO users(email, nickname, password, pfpPath) VALUES(?, ?, ?, ?)';
+
+registerRouter.post('/register', (req, res) => {
+    const userEmail = req.body.userEmail;
+    const userName = req.body.userName;
+    const userPassword = req.body.userPassword;
+    const pfpPath = '/Images/pfps/default_pfp';
+   //console.log(req.body.userName);
+
+    connection.connect();
+    connection.query(registerQuery, [userEmail, userName, userPassword, pfpPath], (err, result) => {
+        if(err) {
+            //handles response for non unique error mysql errorcode #1169
+            if(err.code === 1169) {
+                res.status(500).send(err.message);
+            }
+            res.status(500).send(err);
+            throw err;
+        }
+        else {
+            res.status(200).send('Successfully registered');
+        }
+    });
+});
+
+module.exports = registerRouter;
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.sendStatus(403);
-});
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
+
+app.use('/users', loginRouter, registerRouter);
